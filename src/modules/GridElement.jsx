0f1ac0f73eed5092713e501b8b3805c82f100c57
Joaquín Carreño <joaquin.carreno.h@ug.uchile.cl>
2024-07-23 10:49:01 -0400

    feat: nombres con mayúsculas

--- src/modules/GridElement.jsx
+++ src/modules/GridElement.jsx
@@ -1,11 +1,16 @@
 import "./GridElement.css";
 
+const capitalizeFirstLetter = (s) => {
+  return s[0].toUpperCase() + s.slice(1);
+};
+
 function GridElement({ pokemon }) {
   console.log(pokemon.id, pokemon.name);
   console.log(pokemon);
   return (
     <div className="element-container">
-      {pokemon.name} <img src={pokemon.sprites.front_default} />{" "}
+      {capitalizeFirstLetter(pokemon.name)}
+      <img src={pokemon.sprites.front_default} />{" "}
     </div>
   );
 }
